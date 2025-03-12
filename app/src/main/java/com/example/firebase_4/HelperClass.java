3227f2b24ceeecd5d1251cbcbfcb6589b44844fa
Harshad <kadamharshad00@gmail.com>
2025-03-12 21:51:14 +0530

    Login & Sign up page add

---
+++ app/src/main/java/com/example/firebase_4/HelperClass.java
@@ -0,0 +1,57 @@
+package com.example.firebase_4;
+
+public class HelperClass {
+    String name,phone,department,username,password;
+
+
+    public HelperClass() {
+    }
+
+    public HelperClass(String name, String phone, String department, String username, String password) {
+        this.name = name;
+        this.phone = phone;
+        this.department = department;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
